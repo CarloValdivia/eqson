752fdee7be8c064f69d57d0887c9477329f58bb0
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-12 00:08:48 -0500

    Usando plantillas e incluyendo en página principal

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <link href="app/estilos/principal.css" rel="stylesheet" type="text/css">
+    <title>Eqson: Venta de equipos de sonido</title>
+  </head>
+  <?
+    include 'vistas/cabeceraPagina.html';
+  ?>
+
+  <main></main>
+
+  <?
+    include 'vistas/piePagina.html';
+  ?>
+  <body>
+    <script src="app/scripts/jquery.js"></script>
+    <script src="app/scripts/principal.js"></script>
+  </body>
+</html>
