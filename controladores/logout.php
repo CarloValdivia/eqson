e2df8cb02bd442477d9a94bbb24ea9052d20af57
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-12 23:27:34 -0500

    Cambindo keys de $_SESSION a español

--- controladores/logout.php
+++ controladores/logout.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
 
-    unset($_SESSION['username']);
-    unset($_SESSION['admin']);
+    unset($_SESSION['usuario']);
+    unset($_SESSION['privilegio']);
     header('Location: ../index.php');
-?>
