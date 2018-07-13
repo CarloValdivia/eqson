ece4ca3bf6af99b1a76b85a775fc523e10b65f9b
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-12 23:30:16 -0500

    Clase modelo Productos

---
+++ modelos/Productos.php
@@ -0,0 +1,23 @@
+<?php
+class Productos {
+    protected $conexion = null;
+
+    public function conectar()
+    {
+        $this->conexion = new PDO("mysql:host=localhost;dbname=eqson", "root", "");
+    }
+
+    public function listarProductos()
+    {
+        $consulta = $this->conexion->prepare("SELECT * FROM Producto");
+        $consulta->execute();
+
+        return $consulta;
+    }
+
+    public function agregarProducto($marca, $modelo, $imagen, $descripcion, $especificaciones, $precioFabrica, $precioVenta, $stock)
+    {
+        $sql = "INSERT INTO Producto VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $this->conexion->prepare($sql)->execute([$marca, $modelo, $imagen, $descripcion, $especificaciones, $precioFabrica, $precioVenta, $stock]);
+    }
+}
