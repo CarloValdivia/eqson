e455d0cb83082ce6b6af7f6239e4406a5a9e1b1d
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-12 23:30:37 -0500

    Controlador de inventario de productos

---
+++ controladores/almacen.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../modelos/Productos.php';
+
+$data = new Productos();
+$data->conectar();
+
+// Agregando productos
+$marca =  $_POST['marca_producto'] ?? '';
+$modelo =  $_POST['modelo_producto'] ?? '';
+$imagen =  $_POST['imagen_producto'] ?? '';
+$descripcion =  $_POST['descripcion_producto'] ?? '';
+$especificaciones =  $_POST['especificaciones_producto'] ?? '';
+$precioFabrica =  $_POST['precio_fabrica'] ?? '';
+$precioVenta =  $_POST['precio_venta'] ?? '';
+$stock =  $_POST['cantidad'] ?? '';
+
+$data->agregarProducto($marca, $modelo, $imagen, $descripcion, $especificaciones, $precioFabrica, $precioVenta, $stock);
+header('Location: ../vistas/almacen.html');
+
