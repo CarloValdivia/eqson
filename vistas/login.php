f63d103a2d3fcc590d8d330a42f637d2aa15aa47
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-12 23:20:58 -0500

    Agregando ayuda para usuario no registrado o clave incorrecta

--- vistas/login.php
+++ vistas/login.php
@@ -1,3 +1,35 @@
+<?php
+session_start();
+
+require '../modelos/Usuarios.php';
+
+$data = new Usuarios();
+$data->conectar();
+
+$nombre = $_POST['nombre_usuario'] ?? '';
+$clave = $_POST['contrasena'] ?? '';
+
+$claveIncorrecta = false;
+$usuarioNoExiste = false;
+
+if ($nombre and $clave) {
+    $resultado = $data->encontrarUsuario($nombre);
+    $filas = $resultado->rowCount();
+    if ($filas > 0) {
+        $record = $resultado->fetch(PDO::FETCH_ASSOC);
+        if (strcmp($clave, $record['clave']) === 0) {
+            $_SESSION['usuario'] = $nombre;
+            $_SESSION['privilegio'] = $record['esAdmin'];
+            header('Location: ../index.php');
+        } else {
+            $claveIncorrecta = true;
+        }
+    } else {
+        $usuarioNoExiste = true;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -17,43 +49,17 @@
     <div class="login">
       <div class="login-triangle"></div>
       <h2 class="login-header">Iniciar Sesión</h2>
-      <form class="login-container" action="../controladores/login.php" method="POST">
+      <form class="login-container" action="" method="POST">
         <p><input type="text" placeholder="Usuario" name="nombre_usuario" required></p>
         <p><input type="password" placeholder="Contraseña" name="contrasena" required></p>
         <p><input type="submit" value="Entrar"></p>
+          <? if ($claveIncorrecta): ?>
+          <p style="color: red;">Contraseña incorrecta</p>
+          <? endif; ?>
+          <? if ($usuarioNoExiste): ?>
+          <p style="color: red;">No existe este usuario</p>
+          <? endif; ?>
       </form>
-
       <p><a href="../index.php">Página principal</a></p>
   </body>
 </html>
-<?php
-session_start();
-
-require '../modelos/Usuarios.php';
-
-$data = new Usuarios();
-$data->conectar();
-
-$nombre = $_POST['nombre_usuario'];
-$clave = $_POST['contrasena'];
-
-$resultado = $data->encontrarUsuario($nombre);
-$count = $resultado->rowCount();
-if ($count > 0)
-{
-  $record = $resultado->fetch(PDO::FETCH_ASSOC);
-  if (strcmp($clave, $record['clave']) === 0) {
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $nombre;
-    $_SESSION['admin'] = $record['esAdmin'];
-    header('Location: ../index.php');
-  } else {
-    echo "Contraseña incorrecta!";
-    echo "<script>setTimeout(\"location.href = '../vistas/login.html';\",1000);</script>";
-  }
-} else {
-  echo "Usuario no registrado!";
-  echo "<script>setTimeout(\"location.href = '../vistas/login.html';\",1000);</script>";
-}
-
-?>
