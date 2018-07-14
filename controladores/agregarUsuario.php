b50b25efa1c6e853c8ea8dc1f0707a876e34c9d0
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-13 19:36:01 -0500

    Controlador para agregar usuarios (Administradores y clientes)

---
+++ controladores/agregarUsuario.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../modelos/Usuarios.php';
+
+$data = new Usuarios();
+$data->conectar();
+
+$nombre = $_POST['nombre_usuario'];
+$clave = $_POST['contrasena_usuario'];
+// Si es que no especifica domicilio asignarle $x = ""
+$direccion = $_POST['direccion_domicilio'] ?? '';
+$privilegio = $_POST['privilegio'];
+
+$resultado = $data->agregarUsuario($nombre, $clave, $direccion, $privilegio);
+
+if ($resultado)  {
+    header('Location: ../vistas/usuarios.php');
+} else {
+    header('Location: ../vistas/usuarios.php?usuario=repetido');
+}
