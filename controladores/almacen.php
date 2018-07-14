35841f3528d877c55ac39415a5e92de1f7370ec5
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-13 19:32:23 -0500

    null coalesce operadores solo en variables que son opcionales

--- controladores/almacen.php
+++ controladores/almacen.php
@@ -6,14 +6,15 @@ $data = new Productos();
 $data->conectar();
 
 // Agregando productos
-$marca =  $_POST['marca_producto'] ?? '';
-$modelo =  $_POST['modelo_producto'] ?? '';
-$imagen =  $_POST['imagen_producto'] ?? '';
-$descripcion =  $_POST['descripcion_producto'] ?? '';
+$marca =  $_POST['marca_producto'];
+$modelo =  $_POST['modelo_producto'];
+$imagen =  $_POST['imagen_producto'];
+$descripcion =  $_POST['descripcion_producto'];
+// Si es que no recibe un valor darle un ''
 $especificaciones =  $_POST['especificaciones_producto'] ?? '';
-$precioFabrica =  $_POST['precio_fabrica'] ?? '';
-$precioVenta =  $_POST['precio_venta'] ?? '';
-$stock =  $_POST['cantidad'] ?? '';
+$precioFabrica =  $_POST['precio_fabrica'];
+$precioVenta =  $_POST['precio_venta'];
+$stock =  $_POST['cantidad'];
 
 $data->agregarProducto($marca, $modelo, $imagen, $descripcion, $especificaciones, $precioFabrica, $precioVenta, $stock);
 header('Location: ../vistas/almacen.html');
