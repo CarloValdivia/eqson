58535699a7146d60dac6e4f092df6ca5bdcb296e
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-15 13:33:14 -0500

    Link a página principal index.php

--- vistas/login.php
+++ vistas/login.php
@@ -16,20 +16,22 @@
   </style>
 </head>
 <body>
-<div class="login">
-  <div class="login-triangle"></div>
-  <h2 class="login-header">Iniciar Sesión</h2>
-  <form class="login-container" action="../controladores/checkLogin.php" method="POST">
-    <p><input type="text" placeholder="Usuario" name="nombre_usuario" required></p>
-    <p><input type="password" placeholder="Contraseña" name="contrasena" required></p>
-    <p><input type="submit" value="Entrar"></p>
-      <? if (isset($_GET["clave"]) && $_GET["clave"] == 'incorrecta'): ?>
+  <div class="login">
+    <div class="login-triangle"></div>
+    <h2 class="login-header">Iniciar Sesión</h2>
+    <form class="login-container" action="../controladores/checkLogin.php" method="POST">
+      <p><input type="text" placeholder="Usuario" name="nombre_usuario" required></p>
+      <p><input type="password" placeholder="Contraseña" name="contrasena" required></p>
+      <p><input type="submit" value="Entrar"></p>
+      <?php if (isset($_GET["clave"]) && $_GET["clave"] == 'incorrecta'): ?>
         <p style="color: red;">Contraseña incorrecta</p>
-      <? endif; ?>
-      <? if (isset($_GET["usuario"]) && $_GET["usuario"] == 'desconocido'): ?>
+      <?php endif; ?>
+      <?php if (isset($_GET["usuario"]) && $_GET["usuario"] == 'desconocido'): ?>
         <p style="color: red;">No existe este usuario</p>
-      <? endif; ?>
-  </form>
+      <?php endif; ?>
+    </form>
+  </div>
+
   <p><a href="../index.php">Página principal</a></p>
 </body>
 </html>
--- vistas/usuarios.php
+++ vistas/usuarios.php
@@ -8,60 +8,63 @@
   <link href="../app/estilos/formularios.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<form action="../controladores/agregarUsuario.php" method="POST">
-  <h1>Registro de usuarios</h1>
-  <p>Campos requeridos son marcados con un <strong><abbr title="requerido">*</abbr></strong>.</p>
-  <section>
-    <h2>Información general</h2>
-    <fieldset>
-      <legend>Privilegios</legend>
-      <ul>
-        <li>
+  <form action="../controladores/agregarUsuario.php" method="POST">
+    <h1>Registro de usuarios</h1>
+    <p>Campos requeridos son marcados con un <strong><abbr title="requerido">*</abbr></strong>.</p>
+    <section>
+      <h2>Información general</h2>
+      <fieldset>
+        <legend>Privilegios</legend>
+        <ul>
+          <li>
           <label for="administrador">
             <input type="radio" id="administrador" name="privilegio" value="1" required>
             Administrador
           </label>
-        </li>
-        <li>
-          <label for="cliente">
-            <input type="radio" id="cliente" name="privilegio" value="0" required>
-            Usuario normal (Cliente)
-          </label>
-        </li>
-      </ul>
-    </fieldset>
-    <p>
-      <label for="nombre">
-        <span>Nombre: </span>
-        <strong><abbr title="requerido">*</abbr></strong>
-      </label>
-      <input type="text" id="nombre" name="nombre_usuario" required>
-    </p>
-      <? if (isset($_GET["usuario"]) && $_GET["usuario"] == 'repetido'): ?>
-        <p style="color:red;">
-          Ya existe este usuario
-        </p>
+          </li>
+          <li>
+            <label for="cliente">
+              <input type="radio" id="cliente" name="privilegio" value="0" required>
+              Usuario normal (Cliente)
+            </label>
+          </li>
+        </ul>
+      </fieldset>
+      <p>
+        <label for="nombre">
+          <span>Nombre: </span>
+          <strong><abbr title="requerido">*</abbr></strong>
+        </label>
+        <input type="text" id="nombre" name="nombre_usuario" required>
+      </p>
+      <?php if (isset($_GET["usuario"]) && $_GET["usuario"] == 'repetido'): ?>
+      <p style="color:red;">
+        Ya existe este usuario
+      </p>
       <? endif; ?>
-    <p>
-      <label for="contrasena">
-        <span>Contraseña: </span>
-        <strong><abbr title="requerido">*</abbr></strong>
-      </label>
-      <input type="password" id="contrasena" name="contrasena_usuario" required">
-    </p>
-  </section>
-  <section>
-    <h2>Información adicional</h2>
-    <p>
-      <label for="direccion">
-        <span>Dirección del domicilio:</span>
-      </label>
-      <textarea id="direccion" name="direccion_domicilio"></textarea>
-    </p>
-  </section>
-  <section>
-    <p class="button"> <button type="submit">Registrar usuario</button></p>
-  </section>
-</form>
+      <p>
+        <label for="contrasena">
+          <span>Contraseña: </span>
+          <strong><abbr title="requerido">*</abbr></strong>
+        </label>
+        <input type="password" id="contrasena" name="contrasena_usuario" required">
+      </p>
+    </section>
+    <section>
+      <h2>Información adicional</h2>
+      <p>
+        <label for="direccion">
+          <span>Dirección del domicilio:</span>
+        </label>
+        <textarea id="direccion" name="direccion_domicilio"></textarea>
+      </p>
+    </section>
+    <section>
+      <p class="button"> <button type="submit">Registrar usuario</button></p>
+    </section>
+  </form>
+  <p>
+    <a href="../index.php">Página principal</a>
+  </p>
 </body>
 </html>
