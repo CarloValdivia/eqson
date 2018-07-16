30541cc254ca1486b640333c4eb3a5516e8b0ae9
Jerson Zuniga <jzunigacoayla@gmail.com>
2018-07-16 09:44:11 -0500

    Arreglando indentación

--- controladores/logout.php
+++ controladores/logout.php
@@ -1,6 +1,8 @@
 <?php
-    session_start();
 
-    unset($_SESSION['usuario']);
-    unset($_SESSION['privilegio']);
-    header('Location: ../index.php');
+session_start();
+
+unset($_SESSION['usuario']);
+unset($_SESSION['privilegio']);
+
+header('Location: ../index.php');
